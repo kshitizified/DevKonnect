b6f6da4be4d4d34dbcf540d469cc3d2a6c8e9e78
Kshitiz Kumar <kshitiz.kumar@pearson.com>
2020-09-23 09:29:02 +0530

    Making app ready to deploy

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const connectDB = require('./config/db');
 const app = express();
+const path = require('path');
 
 // Connect Database
 connectDB();
@@ -8,13 +9,21 @@ connectDB();
 // Initialize Middlewares
 app.use(express.json({ exended: false }));
 
-app.get('/', (req, res) => res.send('API RUNNING'));
-
 // Define Routes
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/posts', require('./routes/api/posts'));
 app.use('/api/profile', require('./routes/api/profile'));
+
+// Serve static assets in prod
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
